925fb688af0b48cced84df40f8ed1a3057572668
sanjiv sahayam <sanjsmailbox@gmail.com>
2016-05-29 22:21:42 +1000

    add wip dsl

--- src/main/scala/net/ssanj/dabble/DabbleHistoryDsl.scala
+++ src/main/scala/net/ssanj/dabble/DabbleHistoryDsl.scala
@@ -5,6 +5,8 @@ import scala.util.Try
 import scalaz._
 import scalaz.Free._
 
+//Free only encapsulates side effects. Not logic. Logic is performed in the interpreter.
+//Free Scipts have to return a Free[DabbleHistory, ?]
 
 object DabbleHistoryDslDef {
 
@@ -13,70 +15,77 @@ object DabbleHistoryDslDef {
 
   sealed trait HistoryChoice
   final case object ExitHistory extends HistoryChoice //q
-  final case class Unhandled(input: String) extends HistoryChoice //unhandled input
+  final case class UnhandledInput(value: String) extends HistoryChoice //q
   final case class HistoryLine(line: OneBased) extends HistoryChoice //valid menu index
 
   sealed trait DabbleHistoryDsl[A]
-  final case class ReadHistoryFile(filename: String) extends DabbleHistoryDsl[Seq[DabbleHistoryLine]]
+  final case class ReadHistoryFile(filename: String) extends DabbleHistoryDsl[Seq[String]]
   final case class PrintItem(message: String) extends DabbleHistoryDsl[Unit]
-  final case class ReadUserInput(prompt: String, maxLines: OneBased) extends DabbleHistoryDsl[HistoryChoice]
-  final case class LaunchDabble(line: DabbleHistoryLine) extends DabbleHistoryDsl[Unit]
+  final case class ReadUserInput(prompt: String) extends DabbleHistoryDsl[String]
   final case class Exit(code: Int) extends DabbleHistoryDsl[Unit]
 
   type DabbleScript[A] = Free[DabbleHistoryDsl, A]
 
   //2. Lift functions
-  def readHistoryFile(filename: String): DabbleScript[Seq[DabbleHistoryLine]] =
+  def readHistoryFile(filename: String): DabbleScript[Seq[String]] =
     liftF(ReadHistoryFile(filename))
 
   def printItem(message: String): DabbleScript[Unit] = liftF(PrintItem(message))
 
-  def readUserInput(prompt: String, maxLines: OneBased): DabbleScript[HistoryChoice] =
-    liftF(ReadUserInput(prompt, maxLines))
-
-  def launchDabble(line: DabbleHistoryLine): DabbleScript[Unit] = liftF(LaunchDabble(line))
+  def readUserInput(prompt: String): DabbleScript[String] = liftF(ReadUserInput(prompt))
 
   def exit(code: Int): DabbleScript[Unit] = liftF(Exit(code))
 
   //3. Compose functions
-  def getUserChoice(maxLines: OneBased, hLines: Seq[DabbleHistoryLine]): DabbleScript[Unit] = for {
-    choice <- readUserInput(
-                  s"Please select a number between 1 and ${maxLines.value} or type q to exit",
-                  maxLines)
-    _      <- choice match {
-              case ExitHistory                 => exit(0)
-              case Unhandled(_)                => getUserChoice(maxLines, hLines)
-              case HistoryLine(OneBased(line)) => launchDabble(hLines(line - 1))
-            }
+  def getUserChoice(prompt: String, maxLines: OneBased): DabbleScript[HistoryChoice] =
+    readUserInput(prompt).
+      map {
+            case "q" => ExitHistory
+            case in =>
+              Try(in.toInt).
+                filter(n => n >= 1 && n <= maxLines.value).
+                map(line => HistoryLine(OneBased(line))).
+                getOrElse(UnhandledInput(in))
+      }
+
+  def performUserChoice(choice: HistoryChoice, lines: Seq[DabbleHistoryLine], launchDabble: DabbleHistoryLine => Unit): DabbleScript[Unit] = {
+    choice match {
+      case HistoryLine(OneBased(line)) =>
+        launchDabble(lines(line - 1))
+        exit(0)
+
+      case UnhandledInput(_) => for {
+        _ <- printItem("invalid input")
+        _ <- exit(1)
+      } yield ()
+
+      case ExitHistory => exit(0)
+    }
+  }
+
+  def noHistory: DabbleScript[Unit] = for {
+    _ <- printItem("You have not made history.")
+    _ <- printItem("Dabble writes out a history line when you successfully load a dependency and exit.")
+    _ <- exit(0)
   } yield ()
+
+  def hasHistory(prompt: String, hLines: Seq[DabbleHistoryLine], menu: String, launchDabble: DabbleHistoryLine => Unit): DabbleScript[Unit] = {
+     val maxLines = OneBased(hLines.length + 1)
+     for {
+       _      <- printItem(menu)
+       choice <- getUserChoice(prompt, maxLines)
+       _      <- performUserChoice(choice, hLines, launchDabble)
+     } yield ()
+  }
 
   //4. Program
-  def historyProgram(historyFileName: String): DabbleScript[Unit] = for {
-    hLines <- readHistoryFile(historyFileName)
-    _ <-   if (hLines.nonEmpty) {
-            val menu = "~menu~"
-            val maxLines = OneBased(hLines.length + 1)
-            for {
-              _ <- printItem(menu)
-              _ <- getUserChoice(maxLines, hLines)
-            } yield ()
-          } else {
-            for {
-              _ <- printItem("You have not made history.")
-              _ <- printItem("Dabble writes out a history line when you successfully load a dependency and exit.")
-            } yield ()
-            exit(0)
-          }
+  def historyProgram(historyFileName: String,
+                     hParser: String => DabbleHistoryLine,
+                     hMenu: Seq[DabbleHistoryLine] => String,
+                     prompt: String,
+                     launchDabble: DabbleHistoryLine => Unit): DabbleScript[Unit] = for {
+    lines <- readHistoryFile(historyFileName)
+    hLines = lines.map(hParser)
+    _ <-   if (hLines.nonEmpty) hasHistory(prompt, hLines, hMenu(hLines), launchDabble) else noHistory
   } yield ()
-
-  // def askForHistoryLine(prompt: String, maxLines: OneBased): DabbleScript[HistoryChoice] = for {
-  //   input  <- readUserInput(prompt)
-  //   choice <- if (input == "q") ExitHistory
-  //             else {
-  //               Try(input.toInt).toOption.fold(askForHistoryLine(prompt, maxLines)){ln =>
-  //                 if (ln >= 1 && ln <= maxLines.value) HistoryLine(OneBased(ln))
-  //                 else askForHistoryLine(prompt, maxLines)
-  //               }
-  //             }
-  // } yield choice
 }
