392ae2579db802637eda83c55d0032e488ca2bdc
sanjiv sahayam <sanjsmailbox@gmail.com>
2016-06-08 23:19:56 +1000

    refactor dsl code

---
+++ src/main/scala/net/ssanj/dabble/DabbleConsoleInterpreter.scala
@@ -0,0 +1,54 @@
+package net.ssanj.dabble
+
+import scala.util.Try
+
+import ammonite.ops._
+
+import scalaz._
+import scalaz.Id.Id
+import scalaz.syntax.std.`try`._
+
+import DabbleDslDef._
+
+class DabbleConsoleInterpreter extends (DabbleDsl ~> Id) {
+
+  def apply[A](dsl: DabbleDsl[A]): Id[A] = dsl match {
+
+    case ReadFile(filename: String) =>
+        Try(read.lines(Path(filename), "UTF-8")).
+          toDisjunction.
+          leftMap(x => s"Failed to read $filename due to: ${x.getMessage}")
+
+    case WriteFile(filename: String, lines: Seq[String]) =>
+      Try(write.over(Path(filename), lines.mkString(newline))).
+        toDisjunction.
+        leftMap(x => s"Failed to write to $filename due to: ${x.getMessage}")
+
+    case FileExists(filename: String) =>
+      Try(exists(Path(filename))).toOption.fold(false)(identity)
+
+    case Log(message: String) => println(message)
+
+    case ReadInput(prompt: String) =>
+      println(prompt)
+      scala.io.StdIn.readLine
+
+    case SystemProp(key: String) =>
+      Try(System.getProperty(key)).
+        toDisjunction.
+        leftMap(x => s"Could not read system property: $key due to: ${x.getMessage}")
+
+    case CallProcess(filename: String, arguments: String, workingDir: String) =>
+      Try(%(filename, arguments)(Path(workingDir))).
+      toDisjunction.
+      map(code =>
+          ExecutionResult2(None,
+                          if (code == 0) SuccessfulAction
+                          else UnsuccessfulAction)).
+      leftMap(x => s"Could not run dabble due to: ${x.getMessage}. See sbt log for details.")
+
+    case Exit(er: ExecutionResult2) => System.exit(er.code.code)
+
+    case  NoOp =>
+  }
+}
--- src/main/scala/net/ssanj/dabble/DabbleDslRunner.scala
+++ src/main/scala/net/ssanj/dabble/DabbleDslRunner.scala
@@ -4,12 +4,10 @@ import scala.util.Try
 
 import ammonite.ops._
 
-import scalaz._
+import scalaz.{-\/, \/-}
 import scalaz.syntax.either._
-import scalaz.Id.Id
 import scalaz.NonEmptyList.nels
 import scalaz.syntax.std.`try`._
-
 
 import DabbleDslDef._
 import DabblePaths._
@@ -18,15 +16,15 @@ import DependencyParser._
 import ResolverParser._
 import Banner._
 import TerminalSupport._
-import dsl.CommonCommands._
 import dsl.DependencyCommands._
-import dsl.HistoryCommands._
 
 object DabbleDslRunner extends App {
-  val historyFileName = dabbleHome.history.toString
 
-  parser.parse(args, DabbleRunConfig()) match {
-    case Some(DabbleRunConfig(deps, res, mp, None)) =>
+  private def loadDependencies(drc: DabbleRunConfig, dabbleHome: DabbleHome): Unit = {
+      val deps = drc.dependencies
+      val res  = drc.resolvers
+      val mp   = drc.macroParadiseVersion
+
       val lineE = for {
         dependencies <- parseDependencies(deps)
         resolvers    <- if (res.nonEmpty) parseResolvers(res) else Seq.empty.right[String]
@@ -34,7 +32,7 @@ object DabbleDslRunner extends App {
 
       lineE match {
         case -\/(error) =>
-          s"Could not parse inputs: $error."
+          println(s"Could not parse inputs: $error.")
 
         case \/-(line)  =>
           val argParser: CommandlineParser = historyParser.parse(_, DabbleRunConfig())
@@ -42,7 +40,7 @@ object DabbleDslRunner extends App {
 
           getBanner.foreach(println)
 
-          Try(launchSbtConsole(historyFileName,
+          Try(launchSbtConsole(dabbleHome.history.toString,
                                line,
                                argParser,
                                historyPrinter).
@@ -50,53 +48,17 @@ object DabbleDslRunner extends App {
             toDisjunction.
             fold(x => println(s"dabble failed with ${x.getMessage}"), r => ())
       }
+  }
 
-    case Some(DabbleRunConfig(_, _, _, Some(HistoryCommand(searchTerm)))) => ???
+  private def loadHistory(termOp: Option[String]): Unit = ???
+
+  parser.parse(args, DabbleRunConfig()) match {
+    case Some(drc@DabbleRunConfig(deps, res, mp, None)) =>
+      loadDependencies(drc, dabbleHome)
+
+    case Some(DabbleRunConfig(_, _, _, Some(HistoryCommand(searchTerm)))) =>
+      loadHistory(searchTerm)
 
     case None => //failed to parse options. Parser will indicate error.
-  }
-}
-
-class DabbleConsoleInterpreter extends (DabbleDsl ~> Id) {
-
-  def apply[A](dsl: DabbleDsl[A]): Id[A] = dsl match {
-
-    case ReadFile(filename: String) =>
-        Try(read.lines(Path(filename), "UTF-8")).
-          toDisjunction.
-          leftMap(x => s"Failed to read $filename due to: ${x.getMessage}")
-
-    case WriteFile(filename: String, lines: Seq[String]) =>
-      Try(write.over(Path(filename), lines.mkString(newline))).
-        toDisjunction.
-        leftMap(x => s"Failed to write to $filename due to ${x.getMessage}")
-
-    case FileExists(filename: String) =>
-      Try(exists(Path(filename))).toOption.fold(false)(identity)
-
-    case Log(message: String) => println(message)
-
-    case ReadInput(prompt: String) =>
-      println(prompt)
-      scala.io.StdIn.readLine
-
-    case SystemProp(key: String) =>
-      Try(System.getProperty(key)).
-        toDisjunction.
-        leftMap(x => s"Could not read system property: $key due to ${x.getMessage}")
-
-    case CallProcess(filename: String, arguments: String, workingDir: String) =>
-      Try(%(filename, arguments)(Path(workingDir))).
-      toDisjunction.
-      map(code =>
-          ExecutionResult2(None,
-                          if (code == 0) SuccessfulAction
-                          else UnsuccessfulAction)).
-      leftMap(x => s"Could not run dabble due to ${x.getMessage}. See sbt log for details.")
-
-    case Exit(er: ExecutionResult2) => System.exit(er.code.code)
-
-    case  NoOp =>
-
   }
 }
